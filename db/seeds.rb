8bf06e514be0dba5c985d689d9ceef38a1eca219
hawkes69 <hawkespoulter@gmail.com>
2024-01-27 13:06:38 -0500

    adjust seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,210 +3,210 @@ Attraction.destroy_all
 Attraction.create!([
   {
     name: "Flight of the Hippogriff",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "The Wizarding World of Harry Potter – Hogsmeade",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Hagrid's Magical Creatures Motorbike Adventure",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "The Wizarding World of Harry Potter – Hogsmeade",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Harry Potter and the Escape from Gringotts",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "The Wizarding World of Harry Potter – Diagon Alley",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Hollywood Rip Ride Rockit",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "Production Central",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Revenge of the Mummy: The Ride",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "New York",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "The Incredible Hulk Coaster",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Marvel Super Hero Island",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Troller Coaster",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "DreamWorks Land",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "VelociCoaster",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Jurassic Park",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Despicable Me Minion Mayhem",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "Minion Land on Illumination Ave",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "Harry Potter and the Forbidden Journey",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "The Wizarding World of Harry Potter – Hogsmeade",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "Race Through New York Starring Jimmy Fallon",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "New York",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "The Amazing Adventures of Spider-Man",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Marvel Super Hero Island",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "The Simpsons Ride",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "World Expo/Springfield",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "Transformers: The Ride 3D",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "Production Central",
     attraction_type: "motion simulator",
     completed: false
   },
   {
     name: "Caro–Seuss–el",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Seuss Landing",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Doctor Doom's Fearfall",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Marvel Super Hero Island",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Dudley Do-Right's Ripsaw Falls",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Toon Lagoon",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "E.T. Adventure",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "DreamWorks Land",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Fast & Furious: Supercharged",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "San Francisco",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Hogwarts Express",
-    park: "Universal Studios Florida/Universal Islands of Adventure",
+    park: "Universal Studios/Universal Islands of Adventure",
     location: "Hogsmeade and London/Diagon Alley",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Illumination's Villain-Con Minion Blast",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "Minion Land on Illumination Ave",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Jurassic Park River Adventure",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Jurassic Park",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Kang & Kodos' Twirl 'n' Hurl",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "World Expo",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Men in Black: Alien Attack",
-    park: "Universal Studios Florida",
+    park: "Universal Studios",
     location: "World Expo",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "One Fish, Two Fish, Red Fish, Blue Fish",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Seuss Landing",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Popeye & Bluto's Bilge-Rat Barges",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Toon Lagoon",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Skull Island: Reign of Kong",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Skull Island",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "Storm Force Accelatron",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Marvel Super Hero Island",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "The Cat In The Hat",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "Seuss Landing",
     attraction_type: "ride",
     completed: false
   },
   {
     name: "The High in the Sky Seuss Trolley Train Ride",
-    park: "Universal Islands of Adventure",
+    park: "Islands of Adventure",
     location: "up ur butt",  # Please replace this with the correct location
     attraction_type: "ride",
     completed: false

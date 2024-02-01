20a12fd197280013c1c483c394db739a0d83984a
hawkes69 <hawkespoulter@gmail.com>
2024-02-01 15:33:42 -0500

    front end table filtering

--- mapp/src/components/List.jsx
+++ mapp/src/components/List.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -16,32 +17,47 @@ import { Add } from "@mui/icons-material";
 import ListRow from "./ListRow";
 
 function List({ type, data, isLoading }) {
+  const [query, setQuery] = useState("");
 
   return isLoading ? (
     <div className="flex items-center justify-center h-screen">Loading...</div>
   ) : (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Completed</TableCell>
-            <TableCell>{type}</TableCell>
-            <TableCell>Park</TableCell>
-            <TableCell>Area</TableCell>
-            <TableCell>
-              <Link to={`/${type.toLowerCase()}/new`}>
-                <Add />
-              </Link>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <ListRow key={row.id} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className="flex flex-col gap-4 m-4">
+      <input
+        type="text"
+        placeholder="Filter..."
+        className="p-2 border-2 border-gray-200 rounded-md"
+        style={{ width: "-webkit-fill-available" }}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Completed</TableCell>
+              <TableCell>{type}</TableCell>
+              <TableCell>Park</TableCell>
+              <TableCell>Area</TableCell>
+              <TableCell>
+                <Link to={`/${type.toLowerCase()}/new`}>
+                  <Add />
+                </Link>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.filter((row) =>
+              row.name.toLowerCase().includes(query) ||
+              row.park.toLowerCase().includes(query) ||
+              row.area.toLowerCase().includes(query)
+              ).map((row) => (
+              <ListRow key={row.id} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
 

e40f90bf01fc268fa72132cb9dc36182fe01d85f
hawkes69 <hawkespoulter@gmail.com>
2024-02-07 14:08:09 -0500

    accurate completion percentage

--- mapp/src/components/Completions/CompletionIndicator.jsx
+++ mapp/src/components/Completions/CompletionIndicator.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import IMAGES from "~/images/Images";
 
-function CompletionIndicator({ name }) {
+function CompletionIndicator({ name, percentComplete }) {
+  console.log(name, percentComplete, typeof percentComplete)
   const color = name + "Color";
   const grayscale = name + "Grayscale";
 
   return (
     <div className="w-32 h-32 relative">
-      <div className="overflow-hidden absolute z-10" style={{ maxHeight: `${Math.floor(Math.random() * 100) + 1}%` }}>
+      <div className="overflow-hidden absolute z-10" style={{ maxHeight: `${percentComplete}%` }}>
         <img src={IMAGES[grayscale]} />
       </div>
       <img className="absolute" src={IMAGES[color]} />
@@ -17,6 +18,7 @@ function CompletionIndicator({ name }) {
 
 CompletionIndicator.propTypes = {
   name: PropTypes.string.isRequired,
+  percentComplete: PropTypes.number.isRequired,
 };
 
 export default CompletionIndicator;
--- mapp/src/components/Completions/Completions.jsx
+++ mapp/src/components/Completions/Completions.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
 import CompletionIndicator from "./CompletionIndicator";
 
+import { 
+  useFetchAttractionsQuery,
+  useFetchRestaurantsQuery,
+  useFetchShowsQuery,
+} from "../../store/apis/dappApi";
+
+function calculateCompletionPercentage(list, parkName) {
+  const parkListTotal = list.filter(activity => activity.park === parkName).length;
+  const parkListCompleted = list.filter(activity => activity.park === parkName && activity.completed).length;
+
+  if (parkListTotal === 0) {
+    return 0;
+  }
+  else {
+    return (100 - (parkListCompleted / parkListTotal) * 100);
+  }
+}
+
+function activityTrackerType(parkName) {
+  
+}
+
 function Completions() {
-  return (
+  const { data: attractionsData, isLoading: attractionsLoading } = useFetchAttractionsQuery();
+  const { data: restaurantsData, isLoading: restaurantsLoading } = useFetchRestaurantsQuery();
+  const { data: showsData, isLoading: showsLoading } = useFetchShowsQuery();
+
+  return showsLoading || restaurantsLoading || attractionsLoading ? (
+    <div className="flex items-center justify-center h-screen">Loading...</div>
+  ) : (
     <div className="flex justify-center items-center m-12">
       <div className="flex flex-row justify-center flex-wrap gap-12">
-        <CompletionIndicator name="animalKingdom" />
-        <CompletionIndicator name="epcot" />
-        <CompletionIndicator name="magicKingdom" />
-        <CompletionIndicator name="hollywoodStudios" />
-        <CompletionIndicator name="islandsOfAdventure" />
-        <CompletionIndicator name="universalStudios" />
+        <CompletionIndicator name="animalKingdom" percentComplete={calculateCompletionPercentage(showsData, "Animal Kingdom")} />
+        <CompletionIndicator name="epcot" percentComplete={calculateCompletionPercentage(showsData, "Epcot")} />
+        <CompletionIndicator name="magicKingdom" percentComplete={calculateCompletionPercentage(showsData, "Magic Kingdom")} />
+        <CompletionIndicator name="hollywoodStudios" percentComplete={calculateCompletionPercentage(showsData, "Hollywood Studios")} />
+        <CompletionIndicator name="islandsOfAdventure" percentComplete={calculateCompletionPercentage(showsData, "Islands Of Adventure")} />
+        <CompletionIndicator name="universalStudios" percentComplete={calculateCompletionPercentage(showsData, "Universal Studios")} />
       </div>
     </div>
   );

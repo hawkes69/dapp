fbdfd90fb4f2c1ec97bec39c6f8618816c9f91f5
hawkes69 <hawkespoulter@gmail.com>
2024-03-07 10:45:35 -0500

    update readme and info

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 
-This is a working directory that contains a simple app to track the completion percentage of various amusement parks in Orlando. My wife and I would like to "100%" (complete every attraction, watch every show, and eat at every restaurant) each of the parks and want a simple web app to keep track of our progress. This will be a mobile-first app and I don't plan to have a working desktop implementation as we will almost exclusively access this from our phones while in the parks.
+This is a working directory that contains a simple app to track the completion percentage of various amusement parks in Orlando. My wife and I would like to "100%" (complete every ride, watch every show, and eat at every restaurant) each of the parks and want a simple web app to keep track of our progress. This will be a mobile-first app and I don't plan to have a working desktop implementation as we will almost exclusively access this from our phones while in the parks.
 
 ## Tech Stack
 
@@ -13,7 +13,6 @@ I have not yet hosted a demo site. I will update this section when I have a demo
 
 ## TODO
 
-- change attractions to rides on front end
-- edge case where map and experience are both complete
+- I am aware of an edge case where the animations for area completion and experience completion can play at the same time. It is rare enough that I am not going to bother to fix it. It is a known issue and I am okay with it. For the purposes of this app it's all right. I may come back to it in the future but I'd rather get this up and hosted than spend more time monkeying around with it. 
 
 - HOST!!!!
--- mapp/src/components/pages/Info.jsx
+++ mapp/src/components/pages/Info.jsx
@@ -1,23 +1,27 @@
 function Info() {
   return (
     <div className="flex flex-col p-6">
-      <h1 className="font-bold text-xl w-full">About this App</h1>
-      <br />
+      <h1 className="font-bold text-xl w-full pb-4">About this App</h1>
       <p className="text-gray-600">
         My wife and I recently moved to Orlando. We wanted to "100%" all of the parks here. Meaning we want to complete
         all of the rides, shows, and restaurants at all of the parks. This app was created to help us keep track of
         our progress. It is also a showcase of my web development skillset. 
         <br />
+        The tech stack for this app is: Ruby on Rails, React, Redux, TailwindCSS, and SQLite. This is a mobile first app and does not have a desktop implementation
+        as we will be accessing this app almost exclusively from our phones while in the parks. 
         <br />
-        The tech stack for this app is: Ruby on Rails, React, Redux, TailwindCSS, and SQLite. 
+        Feel free to click around this demo version. There are some fun animations at both area and experience completion.
         <br />
-        <br />
-        Feel free to click around this demo version. There are some fun animations at both area and item completions.
-        <br />
-        <br /> 
         <strong>Note:</strong> You can click on the park icons on the home page to see the progress for each park, as well as get to area 
         completion maps. I didn't feel like putting a link to this page anywhere in the app so if you need to get back to
         it for some reason go to /info.
+        <br />
+        <br /> 
+        <strong>Hawkes Poulter: Full Stack Developer</strong>
+        <br /> 
+        <strong>hawkespoulter@gmail.com</strong>
+        <br /> 
+        <strong>801-404-4932</strong>
       </p>
     </div>
   )
